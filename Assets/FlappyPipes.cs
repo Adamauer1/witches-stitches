9279d2d798dbca2fe52931df8847586e3fbd4b01
Adamauer1 <adamauer1@gmail.com>
2024-12-10 00:55:38 +0100

    adding pipes

---
+++ Assets/FlappyPipes.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyPipes : MonoBehaviour
+{
+    [SerializeField] private Transform topPipe;
+    [SerializeField] private Transform bottomPipe;
+    [SerializeField] private float speed;
+    public float gap;
+
+    [SerializeField] private float leftCameraEdge;
+
+    private void Start(){
+        topPipe.position += Vector3.up * gap / 2;
+        bottomPipe.position += Vector3.down * gap / 2;
+        // leftCameraEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+    }
+
+    private void Update(){
+        transform.position += speed * Time.deltaTime * Vector3.left;
+
+        // if (transform.position.x < leftCameraEdge){
+        //     Destroy(gameObject);
+        // }
+    }
+}
---
+++ Assets/FlappySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using UnityEngine;
+
+public class FlappySpawner : MonoBehaviour
+{
+    [SerializeField] private FlappyPipes flappyPipes;
+    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private float minHeight = -1f;
+    [SerializeField] private float maxHeight = 2f;
+    [SerializeField] private float verticalGap = 3f;
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn()
+    {
+        FlappyPipes pipes = Instantiate(flappyPipes, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+        pipes.gap = verticalGap;
+    }
+}
