a064aa24afdcdde53a1abe1a1b32889f7417677c
Adamauer1 <adamauer1@gmail.com>
2024-12-17 11:33:55 +0100

    testing input

--- Assets/Scripts/PortalTransport.cs
+++ Assets/Scripts/PortalTransport.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,24 @@ using UnityEngine;
 public class PortalTransport : MonoBehaviour
 {
     [SerializeField] int nextLevelIndex;
-
+    [SerializeField] GameObject interactionDisplay;
+    private bool playerInRange;
     private void OnTriggerEnter2D(Collider2D collider){
         //temp
         if (collider.gameObject.CompareTag("Player")){
+            interactionDisplay.SetActive(true);
             //StartCoroutine(GameManager.instance.NextLevel(nextLevelIndex));
-            GameManager.instance.LoadLevel(nextLevelIndex);
+            // GameManager.instance.LoadLevel(nextLevelIndex);
         }
     }
+
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            interactionDisplay.SetActive(false);
+        }
+    }
+    
+    public void On
 }
